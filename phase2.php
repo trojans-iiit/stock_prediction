372e78319253c35c1aa5c04a7c59e9e7345bf6a5
trojans-iiit <61101041+trojans-iiit@users.noreply.github.com>
2020-02-16 11:10:00 +0530

    Add files via upload

---
+++ phase2.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<div class="topnav">
+  		<a class="active" href="project.html">Home</a><br><hr>
+  		
+Amount:  <?php echo $_GET["amount"]; ?><br>
+Year: <?php echo $_GET["year"]; ?>
+
+</body>
+</html>
